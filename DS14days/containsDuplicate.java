2dc43c4cb67bd9dd95ffcd639c1c929122ac447e
Ragini Rani <46533864+Ragini132@users.noreply.github.com>
2021-09-14 23:32:45 -0400

    Create containsDuplicate.java

---
+++ DS14days/containsDuplicate.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int flag=0;
+        
+        HashMap<Integer,Integer> map1=new HashMap<Integer,Integer>();
+        
+        for(int i=0;i<nums.length;i++){
+            if(map1.containsKey(nums[i])){
+                flag=1;
+                break;
+            }
+            else{
+                map1.put(nums[i],1);
+            }
+        }
+        if(flag==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
